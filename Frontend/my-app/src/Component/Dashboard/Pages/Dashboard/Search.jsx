427d2a173f836832be05121ee517721e82dc547f
tishaberchha <tishabercha15@gmail.com>
2024-07-18 12:37:56 +0530

    Your commit message

--- Frontend/my-app/src/Component/Dashboard/Pages/Dashboard/Search.jsx
+++ Frontend/my-app/src/Component/Dashboard/Pages/Dashboard/Search.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Search() {
   return (
-    <div>Search</div>
-  )
+    <Navbar className="bg-body-tertiary justify-cntent-between">
+      <Form inline>
+        <div className='position-relative'>
+        <CiSearch className='position-absolute top-50  start-0'/>
+        <Row className='relative'>
+        <Col xs="auto" className="">
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className=" mr-sm-2 w-32 absolute mx-4"
+            />
+          </Col>
+        </Row>
+        </div>
+      </Form>
+      <Form inline>
+      </Form>
+    </Navbar>
+  );
 }
 
-export default Search
+export default Search;
