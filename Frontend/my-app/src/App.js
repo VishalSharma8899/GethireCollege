4c3f27d1ee51526c37b4c5cedfc53e0094ce63f0
Shwetameena35 <shwetameena302@gmail.com>
2024-07-29 10:57:37 +0530

    Subpages

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './Component/Dashboard/Sidebar.jsx';
 import Dashboard from './Component/Dashboard/Pages/Dashboard/Main.jsx';
 import Collgepart from './Component/Dashboard/Pages/College/collgepart.jsx';
@@ -10,55 +9,46 @@ import Placement from './Component/Dashboard/Pages/Placement/Placement.jsx';
 import Notification from './Component/Dashboard/Pages/Notifications/Notification.jsx';
 import Training from './Component/Dashboard/Pages/Training/Training.jsx';
 import Feedback from './Component/Dashboard/Pages/Feedback/Feedback.jsx';
-import Report from './Component/Dashboard/Pages/Report/Report.jsx'
+import Report from './Component/Dashboard/Pages/Report/Report.jsx';
 import Form from './Component/Profile/Form.jsx';
 import CollegePage from './Component/Profile/CollegePage.jsx';
 import UpdateForm from './Component/Profile/UpdateFrom.jsx';
 import PlacementForm from './Component/Profile/PlacementForm.jsx';
 import EventsAddForm from './Component/Profile/EventsAddForm.jsx';
-import Login from './Component/Login/Login.jsx'
-import Signup from './Component/Login/Signup.jsx'
- 
-import ProtectedRoute from './Component/Login/ProtectedRoute.jsx';
+import Login from './Component/Login/Login.jsx';
+import Signup from './Component/Login/Signup.jsx';
+import TopPerformers from './Component/Dashboard/Pages/Jobs/TopPerformers.jsx';
+import SeeAllCompanies from './Component/Dashboard/Pages/Jobs/SeeAllCompanies.jsx';
+import StudentDetails from './Component/Dashboard/Pages/Jobs/StudentDetails.jsx';
+// import ProtectedRoute from './Component/Login/ProtectedRoute.jsx';
+
 const App = () => {
   return (
-    <>
-    <BrowserRouter>
-    <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={< Signup/>} />
-          <Route path="/demo" element={<Form/>} />
-        <Route path="/collegePage" element={<CollegePage/>} />
-        <Route path="/updateForm" element={<UpdateForm/>} />
-        <Route path="/placementForm" element={<PlacementForm/>} />
-        <Route path="/eventAddForm" element={<EventsAddForm/>} />
-        
-    </Routes>
-   
-      <Sidebar>
-        <Routes>
-        {/* <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup />} /> */}
-       
-       
-        <Route element={<ProtectedRoute/>}/>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/college" element={<Collgepart/>} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/student" element={<StudentData />} />
-          <Route path="/placement" element={<Placement/>} />
-          <Route path="/Notification" element={<Notification/>} />
-          <Route path="/Training" element={<Training/>} />
-          <Route path="/Report" element={<Report/>}/>
-          <Route path="/feedback" element={<Feedback/>} />
-        {/* </Route> */}
-       
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/demo" element={<Form />} />
+        <Route path="/collegePage" element={<CollegePage />} />
+        <Route path="/updateForm" element={<UpdateForm />} />
+        <Route path="/placementForm" element={<PlacementForm />} />
+        <Route path="/eventAddForm" element={<EventsAddForm />} />
 
-        </Routes>
-      </Sidebar>
-    </BrowserRouter>
-    </>
+        <Route path="/" element={<Sidebar><Dashboard /></Sidebar>} />
+        <Route path="/dashboard" element={<Sidebar><Dashboard /></Sidebar>} />
+        <Route path="/college" element={<Sidebar><Collgepart /></Sidebar>} />
+        <Route path="/jobs" element={<Sidebar><Jobs /></Sidebar>} />
+        <Route path="/student" element={<Sidebar><StudentData /></Sidebar>} />
+        <Route path="/placement" element={<Sidebar><Placement /></Sidebar>} />
+        <Route path="/notification" element={<Sidebar><Notification /></Sidebar>} />
+        <Route path="/training" element={<Sidebar><Training /></Sidebar>} />
+        <Route path="/report" element={<Sidebar><Report /></Sidebar>} />
+        <Route path="/feedback" element={<Sidebar><Feedback /></Sidebar>} />
+        <Route path="/jobs/top-performers" element={<Sidebar><TopPerformers /></Sidebar>} />
+        <Route path="/jobs/see-all-companies" element={<Sidebar><SeeAllCompanies /></Sidebar>} />
+        <Route path="/jobs/student-details/:name" element={<Sidebar><StudentDetails /></Sidebar>} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ Frontend/my-app/src/Component/Dashboard/Pages/Jobs/ActiveButton.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { Button, Modal, Form, Input } from 'antd';
+import { AntDesignOutlined } from "@ant-design/icons";
+
+const { TextArea } = Input;
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [form] = Form.useForm();
+
+  const showModal = () => {
+    setOpen(true);
+  };
+
+  const handleOk = () => {
+    form
+      .validateFields()
+      .then(values => {
+        setConfirmLoading(true);
+        console.log('Form values: ', values);
+        setTimeout(() => {
+          setOpen(false);
+          setConfirmLoading(false);
+          form.resetFields();
+        }, 2000);
+      })
+      .catch(info => {
+        console.log('Validate Failed:', info);
+      });
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
+    form.resetFields();
+  };
+
+  return (
+    <>
+      <Button className='mt-6' type="primary" onClick={showModal}>
+        <AntDesignOutlined />
+        Invite
+      </Button>
+      <Modal
+        title="Invitation"
+        open={open}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <Form
+          form={form}
+          layout="vertical"
+          name="invite_form"
+        >
+          <Form.Item
+            name="collegeName"
+            label="College Name"
+            rules={[{ required: true, message: 'Please input the college name!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="location"
+            label="Location"
+            rules={[{ required: true, message: 'Please input the location!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="numberOfStudents"
+            label="Number of Students"
+            rules={[{ required: true, message: 'Please input the number of students!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="branch"
+            label="Branch"
+            rules={[{ required: true, message: 'Please input the branch!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="position"
+            label="Position"
+            rules={[{ required: true, message: 'Please input the position!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="batch"
+            label="Batch"
+            rules={[{ required: true, message: 'Please input the batch!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[{ required: true, message: 'Please input the description!' }]}
+          >
+            <TextArea rows={4} />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default App;
