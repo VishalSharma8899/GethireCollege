56fa7f879ef8ad5be442ae6e488213da77dd3140
Shwetameena35 <shwetameena302@gmail.com>
2024-08-01 16:18:12 +0530

    msg

--- Frontend/my-app/src/Component/Dashboard/Pages/Jobs/CardActive.jsx
+++ Frontend/my-app/src/Component/Dashboard/Pages/Jobs/CardActive.jsx
@@ -21,7 +21,7 @@ function CardActive() {
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     // autoplaySpeed: 1000,
